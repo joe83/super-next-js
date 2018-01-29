cf14876538038256afa277728cdc84898e2be475
mkhuda <mhmmd.huda@gmail.com>
2018-01-29 20:35:21 +0700

    add Layout to component

---
+++ components/SuperLayout.js
@@ -0,0 +1,16 @@
+import Header from './Header'
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #222'
+}
+
+const SuperLayout = (props) => (
+  <div style={layoutStyle}>
+    <Header />
+    {props.children}
+  </div>
+)
+
+export default SuperLayout
