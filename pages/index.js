242f81236dc73a16e6803b24bf5a1a6aef64d017
mkhuda <mhmmd.huda@gmail.com>
2018-02-01 22:20:17 +0700

    modify index for dynamic Link to Post page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Link from 'next/link'
 import SuperLayout from '../components/SuperLayout'
+import PostLink from '../components/PostLink'
 
 const Index = () => (
   <SuperLayout>
-    <p>Hello Super</p>
+    <h1>Hello Super</h1>
+    <ul>
+      <PostLink title="Dynamic One"/>
+      <PostLink title="Dynamic Two"/>
+    </ul>
   </SuperLayout>
 )
 
