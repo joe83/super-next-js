90ed40ae07b0fa3bf1eb1c5fda94b8b454ebb12a
mkhuda <mhmmd.huda@gmail.com>
2018-02-27 22:35:48 +0700

    use slug on client side

--- pages/post.js
+++ pages/post.js
@@ -2,7 +2,7 @@ import SuperLayout from '../components/SuperLayout'
 
 const Post = (props) => (
   <SuperLayout>
-    <h1>{props.url.query.title}</h1>
+    <h1>{props.url.query.slug}</h1>
     <p>This is post page</p>
   </SuperLayout>
 )
