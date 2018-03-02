a4eed1b97c1d16f60fef42882a7ca151b2a4b399
mkhuda <mhmmd.huda@gmail.com>
2018-03-02 23:28:04 +0700

    change slug to id

--- pages/post.js
+++ pages/post.js
@@ -2,7 +2,7 @@ import SuperLayout from '../components/SuperLayout'
 
 const Post = (props) => (
   <SuperLayout>
-    <h1>{props.url.query.slug}</h1>
+    <h1>{props.url.query.id}</h1>
     <p>This is post page</p>
   </SuperLayout>
 )
