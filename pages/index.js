e1b4c162dc9de4ab01dff9a1d393f013411f8c44
mkhuda <mhmmd.huda@gmail.com>
2018-03-02 23:28:04 +0700

    change slug to id

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ const Index = () => (
   <SuperLayout>
     <h1>Hello Super</h1>
     <ul>
-      <PostLink slug="dynamic-one" title="Dynamic One"/>
-      <PostLink slug="dynamic-two" title="Dynamic Two"/>
+      <PostLink id="dynamic-one" title="Dynamic One"/>
+      <PostLink id="dynamic-two" title="Dynamic Two"/>
     </ul>
   </SuperLayout>
 )
