4bf0a4f53df09e0b8ececce3896163f579154394
mkhuda <mhmmd.huda@gmail.com>
2018-03-03 23:38:43 +0700

    test props from render and initial fetch

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,28 @@ import fetch from 'isomorphic-unfetch'
 import PostLink from '../components/PostLink'
 import SuperLayout from '../components/SuperLayout'
 
-const Index = () => (
-  <SuperLayout>
-    <h1>Hello Super</h1>
-    <ul>
-      <PostLink id="dynamic-one" title="Dynamic One"/>
-      <PostLink id="dynamic-two" title="Dynamic Two"/>
-    </ul>
-  </SuperLayout>
-)
+const Index = (props) => {
+  console.log('props from render', props)
+  return (
+    <SuperLayout>
+      <h1>Hello Super</h1>
+      <ul>
+        <PostLink id="dynamic-one" title="Dynamic One"/>
+        <PostLink id="dynamic-two" title="Dynamic Two"/>
+      </ul>
+    </SuperLayout>
+  )
+}
 
 Index.getInitialProps = async function() {
   const respond = await fetch('https://api.tvmaze.com/search/shows?q=casper')
   const data = await respond.json()
 
-  console.log(data)
+  console.log('data from fetch', data)
 
-  return {}
+  return {
+    data
+  }
 }
 
 export default Index
