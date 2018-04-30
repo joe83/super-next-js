1d3c865be96a733fa6dca0c526bbe9d09d8e6f80
mkhuda <mhmmd.huda@gmail.com>
2018-05-01 01:18:46 +0700

    use style jsx in PostLink component

--- components/PostLink.js
+++ components/PostLink.js
@@ -5,6 +5,21 @@ const PostLink = (props) => (
     <Link as={`p/${props.id}`} href={`/post?id=${props.id}`}>
       <a>{props.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: red;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 )
 
