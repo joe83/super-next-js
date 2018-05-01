3090ba5e409b394cc8af918b5362b2880979004d
mkhuda <mhmmd.huda@gmail.com>
2018-05-02 06:30:30 +0700

    remove style jsx on postLink

--- components/PostLink.js
+++ components/PostLink.js
@@ -5,21 +5,6 @@ const PostLink = (props) => (
     <Link as={`p/${props.id}`} href={`/post?id=${props.id}`}>
       <a>{props.title}</a>
     </Link>
-    <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: red;
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
   </li>
 )
 
