b9084e5636868d476229b0fef3650622e048fade
mkhuda <mhmmd.huda@gmail.com>
2018-05-02 06:29:55 +0700

    use spectre tab mode on Header

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,28 @@
 import Link from 'next/link'
+import TitleHeader from './TitleHeader'
 
 const linkStyle = {
   marginRight: 15
 }
 
 const Header = () => (
-    <div>
+  <div>
+    <ul className='tab'>
+      <li className='tab-item'>
         <Link href="/">
           <a style={linkStyle}>Home</a>
         </Link>
+      </li>
+      <li className='tab-item'>
         <Link href="/about">
           <a style={linkStyle}>About</a>
         </Link>
-    </div>
+      </li>
+      <li className='tab-item tab-action'>
+        <TitleHeader />
+      </li>
+    </ul>
+  </div>
 )
 
 export default Header
