5e1d3f694b1bca4e7e3e632ac9eedb7aa728a819
mkhuda <mhmmd.huda@gmail.com>
2018-05-02 18:20:38 +0700

    add search functionality

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 import { withRouter } from 'next/router'
@@ -5,27 +6,74 @@ import HelmetMeta from '../components/HelmetMeta'
 import PostLink from '../components/PostLink'
 import SuperLayout from '../components/SuperLayout'
 
-const Index = (props) => {
-  return (
-    <SuperLayout>
-      <HelmetMeta title='Home'/>
-      <ul className='menu'>
-        <li class="divider" data-content="Pick the Sherlock Movie"/>
-        {props.data.map(({show}) => (
-          <PostLink id={`${show.id}`} key={`${show.id}`} title={`${show.name}`}/>
-        ))}
-      </ul>
-    </SuperLayout>
-  )
-}
+class Index extends React.Component {
 
-Index.getInitialProps = async function() {
-  const respond = await fetch('https://api.tvmaze.com/search/shows?q=sherlock')
-  const data = await respond.json()
+  constructor(props) {
+    super(props);
+    this._handleInput = this._handleInput.bind(this)
+    this._handleSearch = this._handleSearch.bind(this)
+    this._handleKeyPress = this._handleKeyPress.bind(this)
+    this.state = {
+      searchQuery: '',
+      queryData: {}
+    }
+  }
 
-  return {
-    data
+  _handleInput = () => {
+    const searchValue = this.search.value
+    this.setState({
+      searchQuery: searchValue
+    })
+  }
+
+
+  _handleSearch = async () => {
+    const respond = await fetch(`https://api.tvmaze.com/search/shows?q=${this.state.searchQuery}`)
+    const data = await respond.json()
+    this.setState({
+      queryData: data
+    })
+  }
+  _handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+     this._handleSearch()
+    }
+  }
+
+  static async getInitialProps() {
+    const respond = await fetch('https://api.tvmaze.com/search/shows?q=sherlock')
+    const data = await respond.json()
+
+    return {
+      data
+    }
+  }
+
+  render() {
+    const queryData = this.state.queryData
+    const response = (Object.keys(queryData).length === 0) ? this.props.data : queryData;
+    return (
+      <SuperLayout>
+        <HelmetMeta title='Home'/>
+        <div className='input-group'>
+          <span className='input-group-addon'>Search by Movie Name</span>
+          <input type='text' className='form-input' onKeyPress={this._handleKeyPress} ref={input => this.search = input} onChange={this._handleInput}/>
+          <button className='btn btn-primary input-group-btn' onClick={this._handleSearch}>
+            <i className='icon icon-search'></i>
+          </button>
+        </div>
+        <ul className='menu'>
+          <li className='divider' data-content='Pick the Sherlock Movie'/>
+          {
+            response.map(({show}) => (
+            <PostLink id={`${show.id}`} key={`${show.id}`} title={`${show.name}`}/>
+            ))
+          }
+        </ul>
+      </SuperLayout>
+    )
   }
 }
+
 
 export default withRouter(Index)
